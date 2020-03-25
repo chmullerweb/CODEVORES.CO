36e0a581827d1ae6cb59aec299f07047815ea952
chmullerweb <ch.muller.web@gmail.com>
2020-03-25 22:44:40 +0100

    Nettoyage sur la page index

---
+++ script.js
@@ -0,0 +1,5 @@
+// subscribe : au clic, la class hidden se éfface de la section inscription (sectionInscription) et la class opacity_activated s'ajoute à l'id opacity. Attention la class hidden doit être ajoutée à l'élément avant d'afficher un nouveau modal
+// menuBurger : au clic, il y a un menu déroulant qui se affiche ou se retire
+// listJob : au clic, il y a une liste déroulante des métiers présents sur le site
+// connexion : au clic, la classe hidden se efface de la section connexion (sectionConnexion) et la class opacity_activated s'ajoute à l'id opacity
+//formulaireContact : au clic, la classe hidden se efface de la section formulaire (sectionFormulaireContact) et la class opacity_activated s'ajoute à l'id opacity
