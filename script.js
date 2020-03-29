cc0fda4ff9b9cb56a8355bd6d7e3e96518642598
chmullerweb <ch.muller.web@gmail.com>
2020-03-29 15:34:15 +0200

    Finalisation du menuBurger

--- script.js
+++ script.js
@@ -1,8 +1,9 @@
-// subscribe : au clic, la class hidden se éfface de la section inscription (sectionInscription) et la class opacity_activated s'ajoute à l'id opacity. Attention la class hidden doit être ajoutée à l'élément avant d'afficher un nouveau modal
-// menuBurger : au clic, il y a un menu déroulant qui se affiche ou se retire
-// listJob : au clic, il y a une liste déroulante des métiers présents sur le site
-// connexion : au clic, la classe hidden se efface de la section connexion (sectionConnexion) et la class opacity_activated s'ajoute à l'id opacity
-//formulaireContact : au clic, la classe hidden se efface de la section formulaire (sectionFormulaireContact) et la class opacity_activated s'ajoute à l'id opacity
+// menuBurger : au clic, un menu apparait ou disparait
+
+document.querySelector('.menuBurger').addEventListener('click', function(){
+    document.getElementById('conteneur-menu').classList.toggle('hidden');
+});
+
 
 //Tous les éléments 'contactPro' de la page ouvre le modal et ajoute de l'opacité sur la page en fond
 
